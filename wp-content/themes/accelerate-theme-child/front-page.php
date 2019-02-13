03388335dedc15a3c24bcf1b4477d79291625435
Mermaid Princess <juliapannewitz@gmail.com>
2019-02-12 18:10:11 -0800

    fixed syntax error in child front page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -39,7 +39,7 @@ get_header(); ?>
 					<figure>
 						<?php echo wp_get_attachment_image( $image_1, $size ); ?>
 					</figure>
-					<h3  class="homepage-study-names"><a href="<? php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+					<h3  class="homepage-study-names"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 				</li>
 				<?php endwhile; ?>
 			<?php wp_reset_query(); ?>
